1969e0a90af0ca2cb73799eff06de60124a17904
luciano <lucholeyria@gmail.com>
2021-08-21 21:14:11 -0300

    Implementacion de una funcion que permite abrir el modal del carrito

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,18 @@ import { Fragment } from "react";
 
 import classes from "./Header.module.css";
 
-const Header = () => {
+const Header = (props) => {
+   const clickHandler = () => {
+      props.changeModalState(true);
+   };
+
    return (
       <Fragment>
          <div className={classes.header}>
             <h1 className={classes.header__logo}>E-Commerce</h1>
-            <p className={classes.header__carrito}>Mi carrito</p>
+            <p className={classes.header__carrito} onClick={clickHandler}>
+               Mi carrito
+            </p>
          </div>
       </Fragment>
    );
