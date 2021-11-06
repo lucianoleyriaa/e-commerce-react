22235a5bb6c0c43dfd171947b568f0220e118778
lucianoleyriaa <lucho@postcron.com>
2021-11-06 12:38:55 -0300

    Refactorizacion de codigo

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
 import { Fragment } from "react";
-import Header from "./components/Header/Header";
-import Productos from "./components/Products/Products";
-import Footer from "./components/Footer/Footer";
 
-import classes from "./App.module.css";
-import Modal from "./components/Modal/Modal";
-import { useState } from "react";
+import RoutesConfig from "./components/Routes/RoutesConfig";
 
 function App() {
-   const [isOpen, changeModal] = useState(false);
-   const [productsOnCarrito, addProduct] = useState([]);
 
-   const closeModal = () => {
-      changeModal(false);
-   };
 
    return (
       <Fragment>
-         <Header changeModalState={changeModal} modalState={isOpen} />
-         <main className={classes.main}>
-            <Productos addProduct={addProduct} products={productsOnCarrito} />
-         </main>
-         <Footer />
-         {isOpen ? (
-            <Modal
-               closeModal={closeModal}
-               products={productsOnCarrito}
-               addProduct={addProduct}
-            />
-         ) : null}
+         <RoutesConfig />
       </Fragment>
    );
 }
